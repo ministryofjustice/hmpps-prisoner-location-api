4221bf18c611e7a86d0947c1e25d6eeb18a2255e
petergphillips <peter@greenthistle.co.uk>
2024-01-24 14:25:09 +0000

    SDIT-1434: ✨ Add in upload and delete (#29)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerdownloadapi/resource/DownloadResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerdownloadapi/resource/DownloadResource.kt
@@ -1,15 +1,24 @@
 package uk.gov.justice.digital.hmpps.prisonerdownloadapi.resource
 
+import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import kotlinx.coroutines.reactive.awaitFirst
+import org.springframework.core.io.buffer.DataBufferUtils
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.prisonerdownloadapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.prisonerdownloadapi.service.DownloadService
@@ -92,6 +101,30 @@ class DownloadResource(private val downloadService: DownloadService) {
   )
   @PreAuthorize("hasAnyRole('ROLE_PRISONER_DOWNLOAD_UI', 'ROLE_PRISONER_DOWNLOAD__RO')")
   suspend fun downloadFile(@PathVariable filename: String): ByteArray = downloadService.download(filename) ?: throw ExtractFileNotFound()
+
+  @DeleteMapping("/delete/{filename}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Hidden
+  @PreAuthorize("hasRole('ROLE_PRISONER_DOWNLOAD__RW')")
+  // Hidden as not part of standard functionality, but useful if file uploaded accidentally or for testing
+  suspend fun deleteFile(@PathVariable filename: String) {
+    downloadService.delete(filename)
+  }
+
+  @PostMapping("/upload", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Hidden
+  @PreAuthorize("hasRole('ROLE_PRISONER_DOWNLOAD__RW')")
+  // Hidden as not part of standard functionality, but useful if file needs to be uploaded or for testing
+  suspend fun uploadFile(@RequestPart("file") file: FilePart) {
+    val content = file.content().map { dataBuffer ->
+      ByteArray(dataBuffer.readableByteCount()).apply {
+        dataBuffer.read(this)
+        DataBufferUtils.release(dataBuffer)
+      }
+    }.awaitFirst()
+    downloadService.upload(file.filename(), content)
+  }
 }
 
 @Schema(description = "NOMIS Extract downloads")
