236f4cc03de0c6049ee6ded6e2ebc14f5725d1cb
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-02-28 11:30:47 +0000

    SDIT-1336 Latest version of hmpps-kotlin-spring-boot-starter lib (#49)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerlocationapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerlocationapi/config/WebClientConfiguration.kt
@@ -3,25 +3,20 @@ package uk.gov.justice.digital.hmpps.prisonerlocationapi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.netty.http.client.HttpClient
+import uk.gov.justice.hmpps.kotlin.auth.reactiveHealthWebClient
 import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
-  @Value("\${api.base.url.oauth}") private val hmppsAuthEndpoint: String,
+  @Value("\${api.base.url.hmpps-auth}") private val hmppsAuthEndpoint: String,
   @Value("\${api.health-timeout:2s}") val healthTimeout: Duration,
   @Value("\${api.timeout:10s}") val timeout: Duration,
 ) {
 
   @Bean
-  fun hmppsAuthWebClient(builder: WebClient.Builder): WebClient = builder.baseUrl(hmppsAuthEndpoint)
-    .clientConnector(ReactorClientHttpConnector(HttpClient.create().responseTimeout(timeout)))
-    .build()
+  fun hmppsAuthWebClient(builder: WebClient.Builder): WebClient = builder.reactiveHealthWebClient(hmppsAuthEndpoint, timeout)
 
   @Bean
-  fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient = builder.baseUrl(hmppsAuthEndpoint)
-    .clientConnector(ReactorClientHttpConnector(HttpClient.create().responseTimeout(healthTimeout)))
-    .build()
+  fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient = builder.reactiveHealthWebClient(hmppsAuthEndpoint, healthTimeout)
 }
