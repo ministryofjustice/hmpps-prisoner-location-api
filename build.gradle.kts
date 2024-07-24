fbd44226839a7119c0c607d41626d3c38a35d950
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-07-24 12:24:49 +0100

    SDIT-1896: Use latest SQS library with distributed tracing

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.2-beta-3")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.1.0")
 
   implementation("aws.sdk.kotlin:s3:1.2.38")
 
