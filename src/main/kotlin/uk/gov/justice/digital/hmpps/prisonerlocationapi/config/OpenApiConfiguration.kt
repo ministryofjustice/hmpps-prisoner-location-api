896018663952efdf8b8503935c10b29e9bb082e2
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-09-04 18:10:57 +0100

    SDIT-2028: 📝 Improve swagger docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerlocationapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerlocationapi/config/OpenApiConfiguration.kt
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.models.media.StringSchema
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
+import io.swagger.v3.oas.models.tags.Tag
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
@@ -35,18 +36,30 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
         .description("Provides the NOMIS location extract as a download")
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
-    .components(
-      Components().addSecuritySchemes(
-        "bearer-jwt",
-        SecurityScheme()
-          .type(SecurityScheme.Type.HTTP)
-          .scheme("bearer")
-          .bearerFormat("JWT")
-          .`in`(SecurityScheme.In.HEADER)
-          .name("Authorization"),
+    .tags(
+      listOf(
+        Tag().name("Popular")
+          .description("The most popular endpoints. Look here first when deciding which endpoint to use."),
+        Tag().name("Deprecated")
+          .description("Endpoints that should no longer be used and will be removed in a future release"),
+        Tag().name("User interface")
+          .description("Endpoints that are designed for the front end and shouldn't be used without consultation"),
       ),
     )
-    .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
+    .components(
+      Components().addSecuritySchemes(
+        "prisoner-location-ui-role",
+        SecurityScheme().addBearerJwtRequirement("ROLE_PRISONER_LOCATION_UI"),
+      ).addSecuritySchemes(
+        "view-prisoner-location-data-role",
+        SecurityScheme().addBearerJwtRequirement("ROLE_PRISONER_LOCATION__RO"),
+      ),
+    )
+    .addSecurityItem(
+      SecurityRequirement()
+        .addList("prisoner-location-ui-role", listOf("read"))
+        .addList("view-prisoner-location-data-role", listOf("read")),
+    )
 
   @Bean
   fun openAPICustomiser(): OpenApiCustomizer = OpenApiCustomizer {
@@ -68,3 +81,11 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
     }
   }
 }
+
+private fun SecurityScheme.addBearerJwtRequirement(role: String): SecurityScheme =
+  type(SecurityScheme.Type.HTTP)
+    .scheme("bearer")
+    .bearerFormat("JWT")
+    .`in`(SecurityScheme.In.HEADER)
+    .name("Authorization")
+    .description("A HMPPS Auth access token with the `$role` role.")
