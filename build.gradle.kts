74a08441b25574a90e3e33e2693a5a6cd45bb05a
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-03-26 16:42:45 +0000

    SDIT-2685: ⬆️ Upgrade springdoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,19 +18,19 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.2")
 
-  implementation("aws.sdk.kotlin:s3:1.4.45")
+  implementation("aws.sdk.kotlin:s3:1.4.48")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.1")
   // Leaving at 1.43.0 to match the version used in App Insights https://github.com/microsoft/ApplicationInsights-Java/blob/3.6.2/dependencyManagement/build.gradle.kts#L14
   implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.47.0")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.5")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.6")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.2")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.25") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.26") {
     exclude(group = "io.swagger.core.v3")
   }
   testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.29")
