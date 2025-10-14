35d3716329a1a706cd893ead844b4493194520e4
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-10-14 09:52:05 +0100

    SDIT-3111: 🔒️ Upgrade to hmpps plugin 9.1.3 for owasp fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.3"
   kotlin("plugin.spring") version "2.2.20"
 }
 
@@ -22,7 +22,7 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.5.0")
 
-  implementation("aws.sdk.kotlin:s3:1.5.54")
+  implementation("aws.sdk.kotlin:s3:1.5.60")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
   // Leaving at 1.43.0 to match the version used in App Insights https://github.com/microsoft/ApplicationInsights-Java/blob/3.6.2/dependencyManagement/build.gradle.kts#L14
@@ -33,10 +33,10 @@ dependencies {
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.7.0")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.34") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.35") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.38")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.39")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
 }
 
