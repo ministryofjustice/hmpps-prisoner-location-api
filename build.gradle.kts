d3dae321d7b88ec8fa77e6d444c345e84cefa200
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-01-27 09:46:54 +0000

    SDIT-3372: ⬆️ Upgrade to app insights 3.7.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.2"
   kotlin("plugin.spring") version "2.3.0"
 }
 
@@ -22,11 +22,11 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:6.0.0")
 
-  implementation("aws.sdk.kotlin:s3:1.6.1")
+  implementation("aws.sdk.kotlin:s3:1.6.3")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
   // Should match the version used in App Insights pulled in by the plugin - https://github.com/ministryofjustice/hmpps-gradle-spring-boot/blob/main/src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt#L10
-  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.53.0")
+  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.55.0")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.1")
 
