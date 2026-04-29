35b56386d315fff062eb46b25a9b80ea742b32a3
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-04-29 15:45:14 +0100

    SDIT-3749: 📈 Add in micrometer registry to track hikari connections

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,10 @@ dependencies {
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.3")
 
+  val appinsightsCore = "core:2.6.4"
+  implementation("io.micrometer:micrometer-registry-azure-monitor:1.16.5")
+  implementation("com.microsoft.azure:applicationinsights-$appinsightsCore")
+
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.1.1")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
 
